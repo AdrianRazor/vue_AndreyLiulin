9144d946dba49dac0d69844026bcd9c09101664a
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-06-26 16:15:50 +0300

    homework Dropdown and Modal

---
+++ vue_modal/src/components/VDropdown.vue
@@ -0,0 +1,122 @@
+<template>
+  <ul
+    class="select"
+    @click="openDropdown"
+    v-click-outside="closeDropdown"
+    :class="{ open: isOpen }"
+  >
+    <li class="select__header">
+      <span class="select__current">{{ header }}</span>
+      <svg class="select__icon" width="15" height="9" viewBox="0 0 15 8.53">
+        <path
+          d="M10,14.26A1,1,0,0,1,9.3,14L2.79,7.44A1,1,0,0,1,4.2,6L10,11.85,15.8,6a1,1,0,1,1,1.41,1.41L10.7,14A1,1,0,0,1,10,14.26Z"
+          transform="translate(-2.5 -5.74)"
+        ></path>
+      </svg>
+    </li>
+    <li
+      class="select__item"
+      v-for="(item, i) in list"
+      :key="i"
+      @click="$emit('change-header', $event)"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "VDropdown",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+    header: {
+      type: String,
+      default: "Choose something",
+    },
+  },
+
+  data: () => ({
+    isOpen: false,
+  }),
+
+  methods: {
+    openDropdown() {
+      this.isOpen = !this.isOpen;
+    },
+    closeDropdown() {
+      this.isOpen = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.select {
+  width: 100%;
+  max-height: 54px;
+  background-color: #fff;
+  border-radius: 8px;
+  transition: all 0.2s ease;
+  cursor: pointer;
+  user-select: none;
+  overflow: hidden;
+  box-sizing: content-box;
+  box-shadow: 0 0 16px rgba(10, 10, 10, 0.1);
+}
+
+.select.open {
+  max-height: 100%;
+}
+
+.select__header {
+  padding: 16px 32px;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.select__current {
+  color: #565656;
+  transition: all 0.2s ease;
+}
+
+.select__header:hover .select__current,
+.select.open .select__current {
+  color: #c12d2d;
+  transition: all 0.2s ease;
+}
+
+.select__icon {
+  fill: #565656;
+  transition: all 0.2s ease;
+}
+
+.select.open .select__icon {
+  transform: rotate(180deg);
+}
+
+.select__header:hover .select__icon,
+.select.open .select__icon {
+  fill: #c12d2d;
+  transition: all 0.2s ease;
+}
+
+.select__item {
+  padding: 16px 32px;
+  color: #565656;
+  background-color: #fff;
+  border-top: 1px solid #e9e6e6;
+  transition: all 0.2s ease;
+}
+
+.select__item:hover {
+  color: #fff;
+  background-color: #c12d2d;
+  transition: all 0.2s ease;
+}
+</style>
---
+++ vue_modal/src/components/VModal.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="modal" :class="{ active: isOpen }">
+    <div class="modal__content">
+      <slot name="header"></slot>
+      <slot></slot>
+      <slot name="footer"></slot>
+    </div>
+    <slot name="aside"></slot>
+    <button id="close" @click="$emit('close-modal')">
+      <svg viewBox="0 0 20 20">
+        <path
+          d="M10.0006 8.82178L14.1256 4.69678L15.3039 5.87511L11.1789 10.0001L15.3039 14.1251L14.1256 15.3034L10.0006 11.1784L5.8756 15.3034L4.69727 14.1251L8.82227 10.0001L4.69727 5.87511L5.8756 4.69678L10.0006 8.82178Z"
+        ></path>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VModal",
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  data: () => ({}),
+
+  methods: {},
+};
+</script>
+
+<style>
+.modal {
+  max-width: 800px;
+  width: 100%;
+  padding: 24px 40px;
+  border-radius: 24px;
+  overflow: hidden;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
+
+  background-image: url(@/assets/img/bg-modal.jpg);
+  background-repeat: no-repeat;
+  background-position: center top;
+  background-size: cover;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(0);
+  transition: transform 0.3s ease;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.modal.active {
+  transform: translate(-50%, -50%) scale(1);
+  transition: transform 0.3s ease;
+}
+
+#close {
+  width: 32px;
+  height: 32px;
+  background-color: transparent;
+  fill: #cdcdcd;
+  cursor: pointer;
+  transition: all 0.2s ease;
+
+  position: absolute;
+  top: 32px;
+  right: 32px;
+  z-index: 10;
+}
+
+#close:hover {
+  fill: #fff;
+  transition: all 0.2s ease;
+}
+
+/* Header */
+.modal__stat {
+  display: flex;
+  align-items: center;
+}
+
+.modal__stat-icon {
+  width: 32px;
+  height: 32px;
+}
+
+.modal__stat-title {
+  margin-left: 6px;
+  font-size: 24px;
+  letter-spacing: 4px;
+  text-transform: uppercase;
+  color: #fff;
+}
+
+.modal__title {
+  margin-top: 12px;
+  font-size: 64px;
+  line-height: 1;
+  text-transform: uppercase;
+  color: #fff;
+}
+
+.modal__subtitle {
+  margin-top: 8px;
+  font-size: 18px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  color: #a5e0f3;
+}
+
+/* Body */
+.modal__text {
+  margin-top: 32px;
+  font-size: 16px;
+  color: #fff;
+}
+
+.modal__link {
+  display: inline-block;
+  margin-top: 8px;
+  border-bottom: 1px solid #959595;
+  font-size: 14px;
+  color: #959595;
+  text-decoration: none;
+}
+
+.modal__link:hover {
+  color: #fff;
+  border-color: #fff;
+}
+
+/* Footer */
+.modal__footer {
+  margin-top: 32px;
+  display: flex;
+}
+
+.modal__block:not(:first-of-type) {
+  margin-left: 32px;
+}
+
+.modal__headline {
+  font-size: 16px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  color: #959595;
+}
+
+.modal__type {
+  margin-top: 4px;
+  display: flex;
+  align-items: center;
+}
+
+.modal__type-icon {
+  width: 18px;
+  height: 18px;
+}
+
+.modal__type-text {
+  font-size: 14px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  color: #fff;
+  margin-left: 8px;
+}
+
+.modal__complexity {
+  margin-top: 8px;
+  margin-left: 4px;
+  display: flex;
+  align-items: center;
+  gap: 0 12px;
+}
+
+.modal__rhombus {
+  width: 12px;
+  height: 12px;
+  border: 1px solid #fff;
+  transform: rotate(45deg);
+}
+
+.modal__rhombus.fill {
+  background-color: #fff;
+}
+
+/* Aside */
+.modal__img {
+  max-width: 400px;
+  width: 100%;
+}
+</style>
